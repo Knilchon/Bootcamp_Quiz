b1b6cfe7b8b13468b82c695eba26e311d90d3ab2
adraca <adraca@xin720305.asv.local>
2022-09-28 15:11:55 +0200

    fixed quizPage Code smell and reference

--- quizPage.ts
+++ quizPage.ts
@@ -1,28 +1,27 @@
 window.onload = () => {
-    let element = document.getElementById("");      //declares connection to a given div
+    let element = document.getElementById("wrapQuestion");      //declares connection to a given div
 
     let questionElement = document.createElement("label");       //creates a question Element
+    element.appendChild(questionElement);   
 
     let answer1Element = document.createElement("button");       //creates answer Elements
-    let answer2Element = document.createElement("button");       
-    let answer3Element = document.createElement("button");       
-    let answer4Element = document.createElement("button");       
+    element.appendChild(answer1Element); 
+    let answer2Element = document.createElement("button");  
+    element.appendChild(answer2Element);      
+    let answer3Element = document.createElement("button");     
+    element.appendChild(answer3Element);   
+    let answer4Element = document.createElement("button");   
+    element.appendChild(answer4Element);     
 
     let progressElement = document.createElement("label");       //creates a progress Element
+    element.appendChild(progressElement); 
 
     let timeElement = document.createElement("label");       //creates a timer Element
+    element.appendChild(timeElement); 
 
     let submitElement = document.createElement("button");       //creates a submit button Element
-    let nextQuestionElement = document.createElement("button");    //creates a next question Element
-
-    element.appendChild(questionElement);       //appends all Elements to the given div
-    element.appendChild(answer1Element); 
-    element.appendChild(answer2Element); 
-    element.appendChild(answer3Element); 
-    element.appendChild(answer4Element); 
-    element.appendChild(progressElement); 
-    element.appendChild(timeElement); 
     element.appendChild(submitElement); 
+    let nextQuestionElement = document.createElement("button");    //creates a next question Element
     element.appendChild(nextQuestionElement); 
 
 }
