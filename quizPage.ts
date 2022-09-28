233d68849bf57089d14d8541fa252a815f448fa2
adraca <adraca@xin720305.asv.local>
2022-09-28 15:36:03 +0200

    added IDs to all elements in landingpage and quizpage and added secound site to json file

--- quizPage.ts
+++ quizPage.ts
@@ -3,25 +3,33 @@ window.onload = () => {
 
     let questionElement = document.createElement("label");       //creates a question Element
     element.appendChild(questionElement);   
+    questionElement.id = "questionElement"
 
     let answer1Element = document.createElement("button");       //creates answer Elements
     element.appendChild(answer1Element); 
+    answer1Element.id = "answer1Element"
     let answer2Element = document.createElement("button");  
     element.appendChild(answer2Element);      
+    answer2Element.id = "answer2Element";
     let answer3Element = document.createElement("button");     
     element.appendChild(answer3Element);   
+    answer3Element.id = "answer3Element";
     let answer4Element = document.createElement("button");   
     element.appendChild(answer4Element);     
+    answer4Element.id = "answer4Element";
 
     let progressElement = document.createElement("label");       //creates a progress Element
     element.appendChild(progressElement); 
+    progressElement.id = "progressElement";
 
     let timeElement = document.createElement("label");       //creates a timer Element
     element.appendChild(timeElement); 
+    timeElement.id = "timeElement";
 
     let submitElement = document.createElement("button");       //creates a submit button Element
     element.appendChild(submitElement); 
+    submitElement.id = "submitElement";
     let nextQuestionElement = document.createElement("button");    //creates a next question Element
     element.appendChild(nextQuestionElement); 
-
+    nextQuestionElement.id = "nextQuestionElement";
 }
