e87d1cf53d3c464cdde1ba7b24f05926dd1d725b
gandres <gandres@xin720308.asv.local>
2022-09-28 11:06:37 +0200

    Questions array

---
+++ questions.ts
@@ -0,0 +1,23 @@
+type Question = {  
+
+    QuestionID: number,
+    Question: string,
+    Answers: string [],
+    CorrectAnswer: string,
+
+}
+
+var arr: Question[] = [
+{
+    QuestionID: 3,
+    Question: "Welcher Film spielt nicht in der MCU Phase 3?",
+    Answers: [
+        "Spider-Man Homecoming",
+        "Ant Man",
+        "Avengers Endgame",
+        "Black Panther",
+
+    ],
+    CorrectAnswer: "Ant Man",
+}
+];
