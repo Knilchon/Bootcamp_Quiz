ba681b645bc5ab9c07d3864dd5d379cf7e9347eb
adraca <adraca@xin720305.asv.local>
2022-09-29 14:29:37 +0200

    added selectabilty for answer buttons, drafted function for answering submiting and loading next question

--- quizPage.ts
+++ quizPage.ts
@@ -1,4 +1,9 @@
-window.onload = () => {
+window.onload = function(){
+    initilize()
+};
+   
+function initilize(){
+    let prozess = 1;
     let element = document.getElementById("wrapQuestion");      //declares connection to a given div
 
     let questionElement = document.createElement("label");       //creates a question Element
@@ -6,17 +11,25 @@ window.onload = () => {
     questionElement.id = "questionElement"
 
     let answer1Element = document.createElement("button");       //creates answer Elements
+    answer1Element.addEventListener("click",selectFunc);
     element.appendChild(answer1Element); 
     answer1Element.id = "answer1Element"
+
     let answer2Element = document.createElement("button");  
+    answer2Element.addEventListener("click",selectFunc);
     element.appendChild(answer2Element);      
     answer2Element.id = "answer2Element";
+
     let answer3Element = document.createElement("button");     
+    answer3Element.addEventListener("click",selectFunc);
     element.appendChild(answer3Element);   
     answer3Element.id = "answer3Element";
+
     let answer4Element = document.createElement("button");   
+    answer4Element.addEventListener("click",selectFunc);
     element.appendChild(answer4Element);     
     answer4Element.id = "answer4Element";
+
 
     let progressElement = document.createElement("label");       //creates a progress Element
     element.appendChild(progressElement); 
@@ -26,10 +39,39 @@ window.onload = () => {
     element.appendChild(timeElement); 
     timeElement.id = "timeElement";
 
-    let submitElement = document.createElement("button");       //creates a submit button Element
-    element.appendChild(submitElement); 
-    submitElement.id = "submitElement";
     let nextQuestionElement = document.createElement("button");    //creates a next question Element
+    nextQuestionElement.addEventListener("click",nextQuestion);
     element.appendChild(nextQuestionElement); 
     nextQuestionElement.id = "nextQuestionElement";
+
+    let submitElement = document.createElement("button");       //creates a submit button Element
+    submitElement.addEventListener("click",submitFunc);
+    element.appendChild(submitElement); 
+    submitElement.id = "submitElement";
+}
+
+function selectFunc(){
+    if(this.value === "Selected"){
+        this.value = "Unselected";
+    }else{
+        this.value= "Selected";
+    }
+}
+function submitFunc(){
+    //hidSub();
+    compareAnswers();
+    //unhideNext();
+}
+function nextQuestion(){
+    questionCounter();
+
+}
+function questionCounter(){
+    let prozessIntContent: number = +document.getElementById("progressElement").textContent;
+    document.getElementById("progressElement").textContent = prozessIntContent.toString() + "/5";
+    //console.log(newProzess);
+    }
+function compareAnswers(){
+    //compares button labels with given answer and tells them if they should be slected => /bool shouldSelect = true;
+    //compares if shouldSelect is true and if the value is "Selected"
 }
